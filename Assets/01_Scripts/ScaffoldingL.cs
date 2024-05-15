7c7c52e22b227661f6314e6ec8517524960d825b
Cwldb <mozi0810@gmail.com>
2024-05-16 00:22:40 +0900

    기믹발판 추가, 코드 수정

--- Assets/01_Scripts/ScaffoldingL.cs
+++ Assets/01_Scripts/ScaffoldingL.cs
@@ -5,37 +5,21 @@ using UnityEngine;
 public class ScaffoldingL : MonoBehaviour
 {
     private bool _rotateOB = false;
-    private Vector3 originalRotation;
-
-    
-
-    private void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !_rotateOB)
         {
             _rotateOB = true;
-            Debug.Log("dd");
             StartCoroutine(RotateObject());
         }
-    }
-
-    private void CollisonEnter()
-    {
-        
     }
 
     IEnumerator RotateObject()
     {
         yield return new WaitForSeconds(2f);
-        Debug.Log("gg");
-        transform.rotation = Quaternion.Slerp(Quaternion.identity, Quaternion.Euler(0, 0, -90f), 0.5f);
+        transform.rotation = Quaternion.Euler(0, 0, -90);
         yield return new WaitForSeconds(2f);
-        Debug.Log("GG");
         _rotateOB = false;
         transform.rotation = Quaternion.Euler(0, 0, 0);
 
--- Assets/01_Scripts/ScaffoldingR.cs
+++ Assets/01_Scripts/ScaffoldingR.cs
@@ -5,37 +5,21 @@ using UnityEngine;
 public class ScaffoldingR : MonoBehaviour
 {
     private bool _rotateOB = false;
-    private Vector3 originalRotation;
-
     
-
-    private void Update()
-    {
-        CollisonEnter();
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !_rotateOB)
         {
             _rotateOB = true;
-            Debug.Log("dd");
             StartCoroutine(RotateObject());
         }
-    }
-
-    private void CollisonEnter()
-    {
-        
     }
 
     IEnumerator RotateObject()
     {
         yield return new WaitForSeconds(2f);
-        Debug.Log("gg");
         transform.rotation = Quaternion.Euler(0, 0, 90);
         yield return new WaitForSeconds(3f);
-        Debug.Log("GG");
         _rotateOB = false;
         transform.rotation = Quaternion.Euler(0, 0, 0);
 
